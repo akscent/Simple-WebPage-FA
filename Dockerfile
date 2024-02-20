235da51c0c6b2fc7bfc7a26a79df793a2d6242fc
akscent <irinyakov2016@yandex.ru>
2024-02-20 12:23:51 +0700

    configured app for back4app

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,20 @@
-FROM python:3.10.12
+FROM python:3.9.6-alpine
 
+# Set the working directory
+WORKDIR /app
+
+# Set environmental variables
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+# Copy over the requirements file and install the dependencies
+COPY ./requirements.txt .
+RUN pip install --no-cache-dir --upgrade -r ./requirements.txt
+
+# Copy over the source code
 COPY . .
 
-RUN pip install -r requirements.txt
+# Expose the port
+EXPOSE 80
 
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
-
-# sudo lsof -i :80 - check port
-# sudo killall nginx | PID - kill nginx process ar pid of process
-# docker build . --tag fastapi_app && docker run -p 80:80 fastapi_app
--- main.py
+++ main.py
@@ -1,20 +1,67 @@
-from fastapi import FastAPI
+import random
+import re
+import string
+from typing import Optional
 
-from contextlib import asynccontextmanager
+from fastapi import FastAPI, HTTPException, Request, Form
+from pydantic import BaseModel
+from starlette.responses import RedirectResponse
 
-from database import create_tables, delete_tables
-from router import router as tasks_router
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await delete_tables()
-    print("База очищена")
-    await create_tables()
-    print("База готова к работе")
-    yield
-    print("Выключение")
+app = FastAPI(
+    title="back4app-url-shortener",
+    description="simple (in-memory) url shortener service",
+    version="1.0.0",
+)
+url_regex = r"^(http|https)://[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(/\S*)?$"
+urls = {}
 
 
-app = FastAPI(lifespan=lifespan)
-app.include_router(tasks_router)
+def generate_alias():
+    return "".join(random.choice(string.ascii_letters + string.digits) for _ in range(8))
+
+
+def generate_unique_alias():
+    alias = generate_alias()
+    while alias in urls:
+        alias = generate_alias()
+
+    return alias
+
+
+@app.get("/")
+async def root():
+    return {
+        "title": app.title,
+        "description": app.description,
+        "version": app.version,
+    }
+
+
+class ShortenRequest(BaseModel):
+    long_url: str
+    alias: Optional[str] = None
+
+
+@app.post("/shorten")
+async def shorten(request: Request, data: ShortenRequest):
+    if not re.match(url_regex, data.long_url):
+        raise HTTPException(status_code=400, detail="Invalid URL.")
+
+    if data.alias and data.alias in urls:
+        raise HTTPException(status_code=400, detail="This URL alias is already taken.")
+
+    alias = data.alias if data.alias else generate_unique_alias()
+    urls[alias] = data.long_url
+
+    return {
+        "long_url": data.long_url,
+        "shortened_url": f"{request.base_url}{alias}",
+    }
+
+
+@app.get("/{alias}")
+async def shortened_url(alias: str):
+    if alias not in urls:
+        raise HTTPException(status_code=404, detail="Shortened URL not found.")
+
+    return RedirectResponse(urls[alias], status_code=303)
